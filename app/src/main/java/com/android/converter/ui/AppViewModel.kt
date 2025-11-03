d883698d5a2306a05b793d2bfc090ace353836fb
slugcat-dev <57626963+slugcat-dev@users.noreply.github.com>
2025-11-03 14:59:23 +0100

    Refresh currencies in ViewModel

--- app/src/main/java/com/android/converter/ui/AppViewModel.kt
+++ app/src/main/java/com/android/converter/ui/AppViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.converter.repository.CurrencyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.system.measureTimeMillis
 
 @HiltViewModel
 class AppViewModel @Inject constructor(
@@ -20,13 +22,11 @@ class AppViewModel @Inject constructor(
     val uiState = state.asStateFlow()
 
     init { viewModelScope.launch {
-        initCurrencies()
-    } }
-
-    private suspend fun initCurrencies() {
+        // Load the last used currencies from the preferences
         val fromCurrencyCode = sharedPreferences.getString("from-currency", "USD") ?: "USD"
         val toCurrencyCode = sharedPreferences.getString("to-currency", "EUR") ?: "EUR"
 
+        // Init the currency repo and app state
         currencyRepository.init()
         currencyRepository.getCurrencies().collect { currencies ->
             state.update { it.copy(
@@ -36,5 +36,23 @@ class AppViewModel @Inject constructor(
                 toCurrency = currencies.find { currency -> currency.code == toCurrencyCode }
             ) }
         }
+
+        refreshCurrencies()
+    } }
+
+    // Fetch current exchange rates
+    suspend fun refreshCurrencies() {
+        state.update { it.copy(refreshing = true) }
+
+        // Add an artificial delay if the refresh takes less than a second to
+        // avoid quick changes in the UI
+        val timeElapsed = measureTimeMillis {
+            currencyRepository.refreshCurrencies()
+        }
+
+        if (timeElapsed < 1000L)
+            delay(1000L - timeElapsed)
+
+        state.update { it.copy(refreshing = false) }
     }
 }
